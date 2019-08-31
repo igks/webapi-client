3b8b05346dbbe99c65f8659882631a7c1483626f
igks <igks.business@gmail.com>
2019-08-31 08:48:45 +0700

    initialize base project

---
+++ src/app/shared/employee.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  formData: Employee;
+  list: Employee[];
+  readonly rootURL = 'http://localhost:49823/api/';
+
+  constructor(private http: HttpClient) {}
+
+  refreshList() {
+    this.http
+      .get(this.rootURL + 'Employee')
+      .toPromise()
+      .then(res => (this.list = res as Employee[]));
+  }
+
+  postEmployee(formData: Employee) {
+    return this.http.post(this.rootURL + 'Employee', formData);
+  }
+
+  putEmployee(formData: Employee) {
+    return this.http.put(
+      this.rootURL + 'Employee/' + formData.EmployeeID,
+      formData
+    );
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete(this.rootURL + 'Employee/' + id);
+  }
+}
