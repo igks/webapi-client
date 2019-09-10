f000cce57893232db14cb176a08078ce5623d056
igks <igks.business@gmail.com>
2019-09-10 22:28:52 +0700

    add sweetalert2 and tidy up the code

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import { Employee } from 'src/app/shared/employee.model';
 
@@ -7,18 +7,17 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material';
 import { EmployeeComponent } from '../employee/employee.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent implements OnInit, AfterViewInit {
-  constructor(
-    private service: EmployeeService,
-    private dialog: MatDialog //private dialogRef: MatDialogRef<EmployeeComponent>
-  ) {}
+export class EmployeeListComponent implements OnInit {
+  constructor(private service: EmployeeService, private dialog: MatDialog) {}
 
+  // variable declaration
   employeeList = new MatTableDataSource<Employee>();
   searchKey: string;
   displayedColumns: string[] = [
@@ -34,23 +33,20 @@ export class EmployeeListComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   ngOnInit() {
+    // update data then implement the sort and pagination when initialize the application
     this.getAllEmployee();
-  }
-
-  ngAfterViewInit(): void {
     this.employeeList.sort = this.sort;
     this.employeeList.paginator = this.paginator;
     this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
   }
 
+  // request the data from api service
   getAllEmployee() {
-    // this.service.getEmployee().subscribe(data => {
-    //   this.employeeList.data = data as Employee[];
-    // });
     this.service.refreshList();
     this.employeeList = this.service.employeeList;
   }
 
+  // implement filter for search feature
   applyFilter(filterValue: string) {
     this.employeeList.filter = filterValue.trim().toLowerCase();
     if (this.employeeList.paginator) {
@@ -58,41 +54,58 @@ export class EmployeeListComponent implements OnInit, AfterViewInit {
     }
   }
 
+  // populate the data to the form when user modified the data
   populateForm(emp: Employee) {
     this.service.formData = Object.assign({}, emp);
   }
 
+  // do confirm when delete then delete the data
   onDelete(id: number) {
-    if (confirm('Are you sure to delete this record?')) {
-      this.service.deleteEmployee(id).subscribe(() => {
-        this.getAllEmployee();
-      });
-    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(result => {
+      if (result.value) {
+        this.service.deleteEmployee(id).subscribe(() => {
+          this.getAllEmployee();
+          Swal.fire({
+            type: 'success',
+            title: 'Deleted!',
+            text: 'Employee data has been deleted.'
+          });
+        });
+      }
+    });
   }
+
+  // clear the search text with clear button
   onClear() {
     this.searchKey = '';
     this.applyFilter(this.searchKey);
   }
+
+  // add the data to database
   onAdd() {
     this.service.dialogTitle = 'Add New Employee';
     this.service.dialogSubmit = 'SUMBIT';
     this.service.resetForm();
     this.raisedDialog();
-    // const dialogRef = this.dialog.open(EmployeeComponent);
-    // dialogRef.afterClosed().subscribe(() => {
-    //   this.getAllEmployee();
-    // });
   }
+
+  // edit the existing data in database
   onEdit(emp) {
     this.service.dialogTitle = 'Modify Existing Employee';
     this.service.dialogSubmit = 'UPDATE';
     this.populateForm(emp);
     this.raisedDialog();
-    // const dialogRef = this.dialog.open(EmployeeComponent);
-    // dialogRef.afterClosed().subscribe(() => {
-    //   this.getAllEmployee();
-    // });
   }
+
+  // raise the dialog box for add or update data accordingly
   raisedDialog() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -7,34 +7,35 @@ import { MatTableDataSource } from '@angular/material/table';
   providedIn: 'root'
 })
 export class EmployeeService {
+  // variable declaration
   employeeList = new MatTableDataSource<Employee>();
   formData: Employee;
   dialogTitle: string;
   dialogSubmit: string;
 
-  readonly rootURL = 'http://localhost:49823/api/';
+  // define the api url
+  readonly rootURL = 'http://webapiservice.somee.com/api/';
 
   constructor(private http: HttpClient) {}
 
+  // update the data to display in UI
   refreshList() {
-    // this.http
-    //   .get(this.rootURL + 'Employee')
-    //   .toPromise()
-    //   .then(res => (this.list = res as Employee[]));
-
     this.getEmployee().subscribe(data => {
       this.employeeList.data = data as Employee[];
     });
   }
 
+  // request data to server
   getEmployee() {
     return this.http.get(this.rootURL + 'Employee');
   }
 
+  // post data to server
   postEmployee(formData: Employee) {
     return this.http.post(this.rootURL + 'Employee', formData);
   }
 
+  // update data to server
   putEmployee(formData: Employee) {
     return this.http.put(
       this.rootURL + 'Employee/' + formData.EmployeeID,
@@ -42,10 +43,12 @@ export class EmployeeService {
     );
   }
 
+  // delete data on server
   deleteEmployee(id: number) {
     return this.http.delete(this.rootURL + 'Employee/' + id);
   }
 
+  // reset form value
   resetForm() {
     this.formData = {
       EmployeeID: null,
